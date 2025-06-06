8eecbf1f85c6310c1531fc329305b09292ae3dd4
Rajkumar <61075470+Rajkumar-21@users.noreply.github.com>
2025-06-06 15:38:06 +0000

    added

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -1,9 +1,9 @@
-alert("Error message")
+// alert("Error message")
 
 // variable
 var myName = "Rajkumar"
-
+console.log(myName)
 // save inputs from user in variable
-var name = prompt("What is your Name?")
+// var name = prompt("What is your Name?")
 
 
